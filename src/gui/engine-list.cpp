3fae3c344bc3c90ff22e77b50e9da4d0ba2a1b95
Mateusz Krawiec <mateusz.krawiec6@gmail.com>
2015-08-08 10:06:51 +0200

    List of engines stub

---
+++ src/gui/engine-list.cpp
@@ -0,0 +1,1 @@
+#include "gui/engine-list.hpp"
---
+++ src/gui/engine-list.hpp
@@ -0,0 +1,10 @@
+#ifndef ENGINE_LIST_HPP
+#define ENGINE_LIST_HPP
+#include <QListView>
+
+class EngineList : public QListView {
+    Q_OBJECT
+};
+
+#endif
+
